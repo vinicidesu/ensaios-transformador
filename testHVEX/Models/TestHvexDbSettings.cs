05081eef45db016d3614875095065f4224bb6fa6
Vinicius Moraes <viniciusmoraeslima@gmail.com>
2022-10-28 22:58:23 -0300

    FIX: Configurações BD

---
+++ testHVEX/Models/TestHvexDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace testHVEX.Models
+{
+    public class TestHvexDbSettings
+    {
+        public string ConnectionString { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+        public string CollectionName { get; set; } = String.Empty;
+    }
+}
--- testHVEX/Program.cs
+++ testHVEX/Program.cs
@@ -1,3 +1,5 @@
+using testHVEX.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Services.Configure<TestHvexDbSettings>(
+    builder.Configuration.GetSection("TestHvex"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
